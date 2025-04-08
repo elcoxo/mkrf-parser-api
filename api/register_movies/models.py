15886224eb6aa1a9e1707e3058a8042b84a22d7e
elcoxo <rakkii7777@gmail.com>
2025-04-08 23:39:05 +0300

    refactor: changed RegisterMovie model field types

--- api/register_movies/models.py
+++ api/register_movies/models.py
@@ -9,13 +9,13 @@ class RegisterMovie(models.Model):
     cardDate: date = models.DateField(verbose_name='Дата регистрации удостоверения', null=True, blank=True)
     director: str = models.TextField(verbose_name='Режиссер', null=True, blank=True)
     studio: str = models.TextField(verbose_name='Студия-производитель', null=True, blank=True)
-    category: str = models.TextField(verbose_name='Категория', null=True, blank=True)
-    viewMovie: str = models.TextField(verbose_name='Вид фильма', null=True, blank=True)
+    category: str = models.CharField(max_length=100, verbose_name='Категория', null=True, blank=True)
+    viewMovie: str = models.CharField(max_length=100, verbose_name='Вид фильма', null=True, blank=True)
     color: str = models.TextField(verbose_name='Цвет', null=True, blank=True)
     ageCategory: str = models.TextField(verbose_name='Возрастная категория', null=True, blank=True)
     startDateRent: date = models.DateField(verbose_name='Дата начала показа фильма', null=True, blank=True)
     crYearOfProduction: date = models.DateField(verbose_name='Год производства', null=True, blank=True)
-    countryOfProduction: str = models.TextField(verbose_name='Страна производства', null=True, blank=True)
+    countryOfProduction: str = models.CharField(max_length=100, verbose_name='Страна производства', null=True, blank=True)
 
     class Meta:
         db_table = "register_movies_db"
