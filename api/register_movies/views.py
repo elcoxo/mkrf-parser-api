25aa5fa72ebff2cb9862996657da9324e44f4d2c
elcoxo <rakkii7777@gmail.com>
2025-04-10 13:47:40 +0300

    feat: added pagination for RegisterMovie list

--- api/register_movies/views.py
+++ api/register_movies/views.py
@@ -1,9 +1,21 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import filters, status, generics
+from django_filters.rest_framework import DjangoFilterBackend
+from django.contrib.auth.models import User
 
-from rest_framework import filters, generics
-
+from api.register_movies.filters import RegisterMovieFilter
 from api.register_movies.models import RegisterMovie
 from api.register_movies.serializers import RegisterMovieSerializer
+
+
+class RegisterMoviesPagination(PageNumberPagination):
+    page_size = 10
+    max_page_size = 100
+    page_query_param = 'page'
+    page_size_query_param = 'size'
 
 class RegisterMoviesListAPIView(generics.ListCreateAPIView):
     """
@@ -11,4 +23,6 @@ class RegisterMoviesListAPIView(generics.ListCreateAPIView):
     """
     queryset = RegisterMovie.objects.order_by('pk')
     serializer_class = RegisterMovieSerializer
+
+    pagination_class = RegisterMoviesPagination
 
